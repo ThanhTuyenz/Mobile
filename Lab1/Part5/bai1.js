a56b174a4eb8687395e312d0d1e1631ace173773
H82M19\Student <Student>
2024-08-28 11:02:18 +0700

    doing part5 bai1

---
+++ Lab1/Part5/bai1.js
@@ -0,0 +1,5 @@
+var gk="BayemMunich"
+var players1=[gk,"fieldPlayers"*10]
+var players2=["fieldPlayers"*11]
+var allPlayers=[players1,players2]
+var players1Final=[gk,"fieldPlayers"*10,"Thiago","Coutinho","Gnabry","Lewandowski","Muller","Kimmich","Alaba","Boateng","Pavard"]
